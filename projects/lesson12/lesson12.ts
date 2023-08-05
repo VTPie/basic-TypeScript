631ea596b594f6d1db07547d0c02d2b9ab0c279a
Vo Phong <phongwind21092001@gmail.com>
2023-08-05 16:12:03 +0700

    array type

---
+++ projects/lesson12/lesson12.ts
@@ -0,0 +1,11 @@
+//Mix type
+// let test12: (string | number)[] = ['Phong'];
+// test12.push('Wind');
+// test12.push(21);
+// console.log('>>> check array: ', test12);
+
+//Type inference
+let test12 = ['Phong', 12];
+test12.push('Wind');
+test12.push(21);
+console.log('>>> check array: ', test12);
