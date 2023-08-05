ed35208762d2fc5b276a5d1c6256f6bf57f002ae
Vo Phong <phongwind21092001@gmail.com>
2023-08-05 14:59:08 +0700

    string type

---
+++ projects/lesson9/lesson9.ts
@@ -0,0 +1,7 @@
+let name9a: string = "Vo Thanh Phong"
+let name9b: string = 'Wind'
+let name9c: string = "Vo Thanh Phong aka \"Wind\""
+let name9d: string = `Vo Thanh Phong aka 
+"Wind"`
+let name9e: string = `${name9a} aka ${name9b}`
+console.log(">>>check name: ", name9e)
