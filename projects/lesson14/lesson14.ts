392a427bd81e27a92977b762e3758375ab371102
Vo Phong <phongwind21092001@gmail.com>
2023-08-05 18:28:52 +0700

    enum type

---
+++ projects/lesson14/lesson14.ts
@@ -0,0 +1,12 @@
+enum REST {
+    PUSH = 'PUSH',
+    DELETE = 'DELETE',
+    UPDATE = 'UPDATE',
+    GET = 'GET'
+}
+
+let rest1 = REST.PUSH
+let rest2 = REST.DELETE
+let rest3 = REST.UPDATE
+let rest4 = REST.GET
+console.log("rest 1 = ", rest1)
