61b7736473126b83112ac0a19333d4774c714294
Vo Phong <phongwind21092001@gmail.com>
2023-08-05 14:37:08 +0700

    type annotations

---
+++ projects/lesson6/lesson6.ts
@@ -0,0 +1,10 @@
+//Type Annotations
+
+//Primitive data types: string, number, boolean, null, undefined, symbol, bigint
+// let count: number = 1;
+// console.log(">>> check count: ", count)
+
+//References data types: object, array, function
+let names2: string[] = ['Vo', 'Thanh', 'Phong']
+names2.push("UIT") //error ?
+console.log(">>> check name 2: ", names2)
